01a4ca294df1c00aaa1971e356aa1b095b9ec74b
windows10 <windows10@DESKTOP-4NS5B06>
2023-06-30 10:09:55 -0600

    Agregar archivos de proyecto.

---
+++ SimpleProjectManager.Module/BusinessObjects/ProjectTask.cs
@@ -0,0 +1,32 @@
+﻿using DevExpress.ExpressApp.DC;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl.EF;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleProjectManager.Module.BusinessObjects
+{
+    [NavigationItem("Planning")]
+    public class ProjectTask : BaseObject
+    {
+        [FieldSize(255)]
+        public virtual string Subject { get; set; }
+
+        public virtual ProjectTaskStatus Status { get; set; }
+
+        public virtual Employee AssignedTo { get; set; }
+
+        [StringLength(4096)]
+        public virtual string Notes { get; set; }
+
+        public virtual Project Project { get; set; }
+
+    }
+
+    public enum ProjectTaskStatus
+    {
+        NotStarted = 0,
+        InProgress = 1,
+        Completed = 2,
+        Deferred = 3
+    }
+}
---
+++ SimpleProjectManager.Module/Migrations/20230629185642_Migracion1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SimpleProjectManager.Module.Migrations
+{
+    public partial class Migracion1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "Testimonials");
+
+            migrationBuilder.DropColumn(
+                name: "DueDate",
+                table: "ProjectTasks");
+
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "ProjectTasks");
+
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "ProjectTasks");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                table: "Testimonials",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DueDate",
+                table: "ProjectTasks",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                table: "ProjectTasks",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                table: "ProjectTasks",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+    }
+}
